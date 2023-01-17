dc0c29d0d96814c2f0fdf768f31ab9c1942dd9e5
Fxhj0501 <3037989509@qq.com>
2023-01-16 23:28:03 -0500

    setup enviroment finished

--- src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,11 +3,17 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getBotInfo/")
-    public String getBotInfo(){
-        return "hhhh";
+    public Map<String,String> getBotInfo(){
+        Map<String,String> bot1 = new HashMap<>();
+        bot1.put("name","Zijian");
+        bot1.put("rating","1500");
+        return bot1;
     }
 }
